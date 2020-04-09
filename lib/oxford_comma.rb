b820896b8a75d5430a81f449f3c7948284a6c870
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 18:46:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      return array.first
+    when 2
+      return "#{array.first} and #{array.last}"
+    else
+      str = ""
+      array.each do |ele|
+        ele == array.last ? str << "and #{ele}" : str << "#{ele}, "
+      end
+      return str
+  end
 end
