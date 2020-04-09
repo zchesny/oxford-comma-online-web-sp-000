e5aec4675d75fa2448caf3dace2ab038a2764e0f
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 18:41:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      return array.first
+    when 2
+      return "#{array.first} and #{array.last}"
+    when 3
+      return "#{array.first}, #{array.[1]}, and #{array.last}"
+  end
 end
